1dbfff4b7d655ad4fe0f5fcd113583ea580c7722
jared-723 <jarwend723@gmail.com>
2023-09-08 19:38:45 +0000

    fixing social media

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -74,10 +74,10 @@ const ContactMe = () => {
         <h3 className="font-bold">Social media</h3>
         <section className="flex gap-4 lg:flex-col">
           <div className="hover:text-blue-600">
-            <i class="bx bxl-instagram text-2xl"></i>
+            <a href="https://www.linkedin.com/in/jarwend-morales-478b41273" target="blank"><i class='bx bxl-linkedin text-2xl'></i></a>
           </div>
           <div className="hover:text-blue-600">
-            <i class="bx bxl-github text-2xl"></i>
+            <a href="https://github.com/jared-723" target="blank"><i class="bx bxl-github text-2xl"></i></a>
           </div>
         </section>
       </footer>
